8951b0089f0da46f3e4897dc812481b4fbf5fcf4
ilhanbozcan <ilhanbozcann@gmail.com>
2020-12-08 18:02:53 +0300

    video done

--- public/script.js
+++ public/script.js
@@ -1,2 +1,64 @@
 const socket = io('/');
-socket.emit('join-room',ROOM_ID, 10)
+const videoGrid=document.getElementById('video-grid')
+const myPeer = new Peer(undefined,{
+    host: '/',
+    port: '3001'
+});
+
+
+
+
+
+
+
+myPeer.on('open',id=>{
+    console.log('myPeer opened')
+    socket.emit('join-room',ROOM_ID,id)
+})
+var streamer;
+socket.on('open-stream',(clientList,userId)=>{
+    if(clientList ==1 ){
+        const myVideo = document.createElement('video')
+        myVideo.muted = true
+    
+        navigator.mediaDevices.getUserMedia({
+            video:true,
+            audio:true
+        }).then(stream=>{
+            streamer = stream;
+            addVideoStream(myVideo,stream);
+        
+        
+        
+        })
+    }
+    else{
+      
+        connectToNewUser(userId,streamer);
+    }
+
+    
+})
+
+
+
+function connectToNewUser(userId,stream){
+    const call = myPeer.call(userId,stream)
+    const video= document.createElement('video')
+    call.on('stream',userVideoStream=>{
+        addVideoStream(userVideoStream)
+    })
+    call.on('close',()=>{
+        video.remove()
+    })
+}
+
+
+function addVideoStream(video,stream){
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata',()=>{
+        video.play()
+    })
+    videoGrid.append(video);
+}
+
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const {v4:uuidV4} = require('uuid')
 app.set('view engine','ejs')
 app.use(express.static('public'))
 
+const stream = true;
 app.get('/',(req,res)=>{
     res.redirect(301,`/${uuidV4()}`)
 })
@@ -16,7 +17,14 @@ app.get('/:room',(req,res)=>{
 
 io.on('connection',socket=>{
     socket.on('join-room', (roomId,userId)=>{
-        console.log(roomId,userId);
+        console.log(roomId,userId)
+        socket.join(roomId);
+        io.sockets.in(roomId).emit('open-stream',io.sockets.adapter.rooms.get(roomId).size,userId)
+        //socket.to(roomId).broadcast.emit('user-connected',userId)
+       
+      
     })
 })
+
+
 server.listen(3000)
