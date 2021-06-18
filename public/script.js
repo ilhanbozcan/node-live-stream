cbafaf3c0ad8ff470f813b480745f7484b3179d3
ilhanbozcan <ilhanbozcann@gmail.com>
2021-06-18 09:42:13 +0300

    chat and 2 user can stream

--- public/script.js
+++ public/script.js
@@ -1,20 +1,34 @@
-const socket = io('/');
-const videoGrid=document.getElementById('video-grid')
-const myPeer = new Peer(undefined,{
-    host: '/',
-    port: '3001'
-});
-
-
-
-
-myPeer.on('open',id=>{
-    console.log('myPeer opened')
-    socket.emit('join-room',ROOM_ID,id)
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, {
+  host: '/',
+  port: '3001'
 })
+
+
+document.getElementById('nick').innerHTML= `Welcome ${NICK}`
+
+console.log(document.getElementById("form"))
+
+
+var userC = 1;
+
+
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const peers = {}
+
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
 var streamer;
 socket.on('open-stream',(clientList,userId)=>{
-    if(clientList ==1 ){
+  document.getElementById('online').innerHTML= clientList
+
+    if(clientList <3 ){
+      console.log('if girdi')
         const myVideo = document.createElement('video')
         myVideo.muted = true
     
@@ -23,25 +37,33 @@ socket.on('open-stream',(clientList,userId)=>{
             audio:false
         }).then(stream=>{
             streamer = stream;
-            addVideoStream(myVideo,stream);
+            addVideoStream(myVideo,stream,userId);
 
         })
+
+      
+      myPeer.call(userId,streamer)
+
     }
     else{
+      console.log('else girdi')
         myPeer.call(userId,streamer)
         console.log('watcher in')
        
     }
 
     myPeer.on('call',call=>{
-        console.log('calling')
-        call.answer(streamer);
-        const video = document.createElement('video')
-        video.muted = true
-        call.on('stream',userVideoStream=>{
-            addVideoStream(video,userVideoStream)
-        })
-    })
+      console.log('calling')
+      call.answer(streamer);
+      const video = document.createElement('video')
+      video.muted = true
+      call.on('stream',userVideoStream=>{
+          addVideoStream(video,userVideoStream,userId)
+      })
+  })
+
+
+
 
    
 
@@ -49,24 +71,73 @@ socket.on('open-stream',(clientList,userId)=>{
 })
 
 
+socket.on('update-user',count => {
+  document.getElementById('online').innerHTML= count
 
-function connectToNewUser(userId,stream){
-    const call = myPeer.call(userId,stream)
-    const video= document.createElement('video')
-    call.on('stream',userVideoStream=>{
-        addVideoStream(userVideoStream)
-    })
-    call.on('close',()=>{
-        video.remove()
-    })
+})
+
+
+
+socket.on('user-disconnected', (userId,userCount) => {
+document.getElementById('online').innerHTML= userCount
+userC = userCount;
+  console.log('disconnect',userId)
+
+  if (peers[userId]) peers[userId].close()
+})
+
+
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
 }
 
-
-function addVideoStream(video,stream){
-    video.srcObject = stream
-    video.addEventListener('loadedmetadata',()=>{
-        video.play()
-    })
-    videoGrid.append(video);
+function addVideoStream(video, stream,userId) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
 }
 
+socket.on('message-receive',(message,nick)=>{
+  let chat = document.getElementById("messageList")
+
+  let tr = document.createElement('tr');
+
+  let m = document.createElement('th');
+  m.textContent = `${nick} : ${message}`
+  tr.append(m)
+  chat.append(tr)
+  document.getElementById('message').value = ""
+
+})
+
+
+
+socket.on('start-timer',(c)=>{
+ 
+  if(c != 'canceled'){
+    document.getElementById('counter').innerHTML = c 
+  }
+  else{
+    document.getElementById('counter').innerHTML = "Waiting for client"
+  }
+})
+
+
+
+function sendMessage(){
+  let m = document.getElementById('message').value
+
+  socket.emit('send-message',m,NICK,ROOM_ID)
+}
--- server.js
+++ server.js
@@ -5,26 +5,98 @@ const io  = require('socket.io')(server)
 const {v4:uuidV4} = require('uuid')
 app.set('view engine','ejs')
 app.use(express.static('public'))
+const session = require('express-session');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
+app.use(session({secret: 'ssshhhhh'}));
+
+
+const auth2 = (req,res,next)=>{
+    req.session.nick ? next() : res.redirect(301,'/')
+}
 
 const stream = true;
 app.get('/',(req,res)=>{
-    res.redirect(301,`/${uuidV4()}`)
+    res.render('index')
 })
 
-app.get('/:room',(req,res)=>{
-    res.render('room',{roomId:req.params.room})
+app.get('/:room',auth2,(req,res)=>{
+    res.render('room',{roomId:req.params.room,nick:req.session.nick})
 })
 
+app.post('/login',(req,res)=>{
+    console.log(req.body.nick)
+    req.session.nick = req.body.nick
+    if(req.session.nick){
+        res.redirect(301,'/public')
+    }
+})
+
+var started = false;
 io.on('connection',socket=>{
     socket.on('join-room', (roomId,userId)=>{
+
+        console.log(roomId)
         console.log(roomId,userId)
         socket.join(roomId);
+
+        socket.emit('update-user',io.sockets.adapter.rooms.get(roomId).size)
+
+        //
         io.sockets.in(roomId).emit('open-stream',io.sockets.adapter.rooms.get(roomId).size,userId)
+
+        //socket.to(roomId).broadcast.emit('user-connected', userId,io.sockets.adapter.rooms.get(roomId).size)
+        
         //socket.to(roomId).broadcast.emit('user-connected',userId)
+
+        socket.on('send-message',(message,nick)=>{
+            console.log(message)
+            io.sockets.in(roomId).emit('message-receive',message,nick)
+        })
+
+  
+        if(!started){
+            if(userId,io.sockets.adapter.rooms.get(roomId).size > 1){
+                started = true
+                var timeleft = 500;
+
+                var downloadTimer = setInterval(function(){
+                  if(timeleft <= 0){
+                    clearInterval(downloadTimer);
+                  }
+                    let c =500 - timeleft + ' for next round';
+                    console.log(c)
+                    socket.to(roomId).broadcast.emit('start-timer',c)
+
+                  timeleft -= 1;
+                }, 1000)
+                
+            }
+            else{
+                started = false
+                socket.to(roomId).broadcast.emit('start-timer','canceled')
+                clearInterval(downloadTimer);
+            }
+        }
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId,io.sockets.adapter.rooms.get(roomId).size)
+          })
+    })
+
+
+
+
+
+  
        
       
     })
-})
+
+    
 
 
 server.listen(3000)
